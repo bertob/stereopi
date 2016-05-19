2ba5d4e934555b7d428bc22c58b06a5faca0a71e
Tobias Bernard <tobias@zenbook>
2016-05-19 15:42:43 +0200

    [init] add project

---
+++ script.js
@@ -0,0 +1,26 @@
+var flkty;
+var SERVER_IP = "172.24.1.121";
+var PORT = 1337;
+
+window.onload = function() {
+  callServer("stop"); // stop music when the application is opened
+	flkty = new Flickity(".carousel", {
+		initialIndex: 0
+	});
+  flkty.on( 'cellSelect', function() {
+    console.log('Flickity select ' + flkty.selectedIndex);
+    if (flkty.selectedIndex === 0) {
+      callServer("stop");
+    } else {
+      callServer("start");
+    }
+  });
+};
+
+
+function callServer(cmd) {
+  console.log(SERVER_IP + ":" + PORT + "/" + cmd);
+  $.post(
+    "http://" + SERVER_IP + ":" + PORT + "/" + cmd
+  );
+}
---
+++ server.js
@@ -0,0 +1,37 @@
+var http = require('http');
+var url = require('url') ;
+var sys = require('util');
+var process = require('process');
+var exec = require('child_process').exec;
+
+var MUSIC_PATH = "~/Muziko/*/*/*.mp3";
+var SERVER_IP = "192.168.12.16";
+var SERVER_PORT = "1337";
+
+function puts(error, stdout, stderr) {
+  sys.puts(stdout);
+}
+
+http.createServer(function(request, response) {
+  var cmd = request.url.split("/")[1];
+  console.log(cmd);
+  if (cmd === "stop") {
+    stopMusic();
+  }
+  else if (cmd === "start") {
+    startMusic();
+  }
+
+  response.setHeader('Access-Control-Allow-Origin', SERVER_IP + ':' + SERVER_PORT);
+  response.setHeader('Access-Control-Allow-Credentials', true);
+  response.end();
+}).listen(1337);
+
+function startMusic() {
+  console.log("starting!");
+  exec("mplayer " + MUSIC_PATH + " -shuffle", puts);
+}
+function stopMusic() {
+  console.log("stopping!");
+  exec("killall mplayer", puts);
+}
