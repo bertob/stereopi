d24a9140cb4cb72ca5f67dda49866166b15d167b
Tobias Bernard <tobias@zenbook>
2016-06-17 19:55:08 +0200

    [NEW] change everything: new API, add volume control, add prev/next buttons (WIP)

--- script.js
+++ script.js
@@ -3,20 +3,23 @@ var SERVER_IP = "172.24.1.1";
 var PORT = 1337;
 
 window.onload = function() {
-  // callServer("stop"); // stop music when the application is opened
-	flkty = new Flickity(".carousel", {
-		initialIndex: 0
-	});
-  flkty.on( 'cellSelect', function() {
-    console.log('Flickity select ' + flkty.selectedIndex);
-    if (flkty.selectedIndex === 0) {
-      callServer("stop");
-    } else {
-      callServer("start");
-    }
-  });
+  // callServer("pause"); // stop music when the application is opened
 };
 
+$("#play").on("click touchstart", function() {
+	callServer("pause");
+});
+$("#prev").on("click touchstart", function() {
+	callServer("prev");
+});
+$("#next").on("click touchstart", function() {
+	callServer("next");
+});
+$("#volume").on("click touchstart", function() {
+	var vol = document.getElementById("volume").value;
+	callServer("volume/" + vol);
+	console.log("volume/" + vol);
+});
 
 function callServer(cmd) {
   console.log(SERVER_IP + ":" + PORT + "/" + cmd);
--- server.js
+++ server.js
@@ -3,22 +3,31 @@ var url = require('url') ;
 var sys = require('util');
 var exec = require('child_process').exec;
 
-var MUSIC_PATH = "/media/pi/A/music/*/*.mp3";
+var CONTROL_PATH = "/tmp/mplayer-control";
+var MUSIC_PATH = "media/pi/*/*.mp3 media/pi/*/*/*.mp3 media/pi/*/*/*/*.mp3";
 var SERVER_IP = "172.24.1.1";
 var SERVER_PORT = "1337";
 
-function puts(error, stdout, stderr) {
-  sys.puts(stdout);
-}
+// Start music on boot
+exec("mkfifo /tmp/mplayer-control", puts);
+exec("mplayer -slave -input file=" + CONTROL_PATH + " " + MUSIC_PATH + " -shuffle", puts);
 
 http.createServer(function(request, response) {
   var cmd = request.url.split("/")[1];
+  var param = request.url.split("/")[2];
   console.log(cmd);
-  if (cmd === "stop") {
-    stopMusic();
+  if (cmd === "pause") {
+    exec('echo "pause" > ' + CONTROL_PATH, puts);
   }
-  else if (cmd === "start") {
-    startMusic();
+  else if (cmd === "next") {
+    exec('echo "pt_step 1" > ' + CONTROL_PATH, puts);
+  }
+  else if (cmd === "prev") {
+    exec('echo "pt_step -1" > ' + CONTROL_PATH, puts);
+  }
+  else if (cmd === "audio") {
+    var vol = parseFloat(param);
+    exec("amixer set PCM " + vol + "%", puts);
   }
 
   response.setHeader('Access-Control-Allow-Origin', SERVER_IP + ':' + SERVER_PORT);
@@ -26,14 +35,26 @@ http.createServer(function(request, response) {
   response.end();
 }).listen(1337);
 
-function startMusic() {
-  console.log("starting!");
-  exec('echo start >> /home/pi/music.log', puts);
-  exec("killall mplayer", puts);
-  exec("mplayer " + MUSIC_PATH + " -shuffle", puts);
+function puts(error, stdout, stderr) {
+  sys.puts(stdout);
 }
-function stopMusic() {
-  console.log("stopping!");
-  exec('echo stop >> /home/pi/music.log', puts);
-  exec("killall mplayer", puts);
-}
+
+// function pauseMusic() {
+//   console.log("starting!");
+//   // exec('echo start >> /home/pi/music.log', puts);
+// }
+// function nextMusic() {
+//   console.log("stopping!");
+//   // exec('echo stop >> /home/pi/music.log', puts);
+// }
+// function prevMusic() {
+//   console.log("stopping!");
+//   // exec('echo stop >> /home/pi/music.log', puts);
+// }
+
+
+// function stopMusic() {
+//   console.log("stopping!");
+//   exec('echo stop >> /home/pi/music.log', puts);
+//   exec("killall mplayer", puts);
+// }
