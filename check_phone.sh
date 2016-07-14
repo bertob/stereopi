217a76ee520d36ef82dea40ea43c189ee791f036
Tobias Bernard <tobias@zenbook>
2016-07-14 20:03:28 +0200

    [minor] change phone detect api call back to play

--- check_phone.sh
+++ check_phone.sh
@@ -6,7 +6,7 @@ if nmap -sP -sn 172.24.1.0/24 | grep -q --line-buffered $1
 then
   echo "Phone present :D"
   echo "UNPAUSING" >> /home/pi/wifi-controls.log
-  curl -X POST http://localhost:1337/reset
+  curl -X POST http://localhost:1337/play
 else
   echo "Phone absent :("
 fi
