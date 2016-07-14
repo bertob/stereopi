500ef4625ef60c006735649989561ef237799ab7
Tobias Bernard <tobias@zenbook>
2016-07-14 11:10:22 +0200

    [simplify] check_phone, only respond to play, rm ON/OFF param, [rename] start.sh

--- check_phone.sh
+++ check_phone.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 
-status=$2
-echo "$2 $1" >> /home/pi/wifi-controls.log
+echo "$1" >> /home/pi/wifi-controls.log
 
 if nmap -sP -sn 172.24.1.0/24 | grep -q --line-buffered $1
 then
   echo "Phone present :D"
-  if [ "$status" = "OFF" ]
-  then
-    echo "UNPAUSING" >> /home/pi/wifi-controls.log
-    curl -X POST http://localhost:1337/pause
-    # amixer set PCM 100%
-  fi
-  status="ON"
+  echo "UNPAUSING" >> /home/pi/wifi-controls.log
+  curl -X POST http://localhost:1337/play
 else
   echo "Phone absent :("
-  if [ "$status" = "ON" ]
-  then
-  	echo "PAUSING" >> /home/pi/wifi-controls.log
-    curl -X POST http://localhost:1337/pause
-  	# amixer set PCM 0%
-  	#killall -9 mplayer
-  fi
-  status="OFF"
 fi
 
 sleep 1
-./check_phone.sh $1 $status
+./check_phone.sh $1
